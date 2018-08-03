58a71543b8c4c1f1b866e32c035f6242138e44ce
Nathan Davis <gmdev86@gmail.com>
2018-08-02 22:05:10 -0400

    stage 1

---
+++ ShooterGame/ShooterGame/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShooterGame
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Sprite background;
+        private Sprite playerShip;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = new Sprite(Content.Load<Texture2D>("spacebackground"), Vector2.Zero);
+
+            var shipTexture = Content.Load<Texture2D>("spaceship");
+            var yPositionOfShip = graphics.GraphicsDevice.Viewport.Height - shipTexture.Height - 10;
+            var xPositionOfShip = (graphics.GraphicsDevice.Viewport.Width / 2) - (shipTexture.Width / 2);
+
+            playerShip = new Sprite(shipTexture, new Vector2(xPositionOfShip, yPositionOfShip));
+        }
+
+    
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+     
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            background.Draw(spriteBatch);
+            playerShip.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ShooterGame/ShooterGame/Sprite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterGame
+{
+    internal class Sprite
+    {
+        private readonly Texture2D texture;
+        private readonly Vector2 position;
+
+        public Sprite(Texture2D texture2D, Vector2 position)
+        {
+            this.texture = texture2D;
+            this.position = position;
+        }
+
+        internal void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
